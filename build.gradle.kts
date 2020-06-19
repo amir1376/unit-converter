cee87e218412fc05ea3d412960cfb5af2460fd5c
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2020-06-19 05:12:05 +0430

    -add gradle pubish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,23 @@
 plugins {
     `java-library`
     kotlin("jvm") version "1.3.72"
+    `maven-publish`
+}
+ext["localRepoDir"] = File(System.getenv("LOCAL_REPO"))
+val localRepoDir: File by ext
+with(File("gradle/external.gradle.kts")) {
+    if (exists()) { apply(from = this@with.absolutePath) }
 }
 
 group = "ir.amirabdol"
 version = "1.0"
-
 repositories {
     mavenCentral()
 }
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("junit", "junit", "4.12")
 }
-
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
@@ -24,5 +27,25 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+val sourceJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourceJar)
+        }
+        val function: (RepositoryHandler).() -> Unit = {
+            maven {
+                name = "my-repo"
+                localRepoDir.mkdirs()
+                url = localRepoDir.toURI()
+            }
+        }
+        repositories(function)
     }
 }
